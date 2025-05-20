b59dd22d1783a1f1cb00f64337e1400803ab3640
Enma26 <iviidel2609@outlook.com>
2025-05-20 02:21:27 -0500

    menu de cliente y administrador terminado, facturador completo

--- ProyectoFinal/main.py
+++ ProyectoFinal/main.py
@@ -1,9 +1,20 @@
-#importo la libreria sqlite3 para la base de datos
+# Importo la librería sqlite3 para la base de datos
 import sqlite3 as sql
+import os
 
 #creo la base de datos que contiene la tabla de usuarios y productos
 def createdb(DatabaseName):
-    conn =sql.connect(DatabaseName)
+    if os.path.exists(DatabaseName):
+        print(f"Database '{DatabaseName}' already exists. Skipping creation.")
+        return
+    conn = sql.connect(DatabaseName)
+    conn.commit()
+    conn.close()
+
+def delateProduct(name):
+    conn = sql.connect('ProyectoFinal/Users&Stock.db')
+    cursor = conn.cursor()
+    cursor.execute('''DELETE FROM inventario WHERE name = ?''', (name,))
     conn.commit()
     conn.close()
 
@@ -35,7 +46,7 @@ def createTableUsers():
     conn.commit()
     conn.close()
 
-#creo la funcion para insertar los datos del nuevo usuario
+#funcion para insertar los datos del nuevo usuario
 def insertUser(name, password, type):
     conn = sql.connect('ProyectoFinal/Users&Stock.db')
     cursor = conn.cursor()
@@ -46,7 +57,8 @@ def insertUser(name, password, type):
     conn.commit()
     conn.close()
 
-#creo la funcion para insertar los datos del nuevo producto
+#funcion para insertar los datos del nuevo producto
+#se le pasan los datos del producto como parametros
 def insertProduct(name, price, stock):
     conn = sql.connect('ProyectoFinal/Users&Stock.db')
     cursor = conn.cursor()
@@ -57,7 +69,9 @@ def insertProduct(name, price, stock):
     conn.commit()
     conn.close()
 
-#creo la funcion para mostrar los productos
+#funcion para mostrar los productos
+#se conecta a la base de datos y se seleccionan todos los productos
+#se guardan en una variable y se imprimen
 def PrintProducts():
     conn = sql.connect('ProyectoFinal/Users&Stock.db')
     cursor = conn.cursor()
@@ -66,55 +80,147 @@ def PrintProducts():
     for row in rows:
         print(row)
     conn.close()
-def userRequest(x):
+
+#funcion para realizaar la compra de productos
+#se le pide al usuario el nombre del producto y la cantidad
+#se busca el producto en la base de datos y se verifica si hay stock
+#si hay stock se actualiza la base de datos y se muestra el precio total
+#si no hay stock se muestra un mensaje de error
+#si el usuario ingresa "salir" se sale del bucle
+#se muestra la boleta de compra con los productos comprados y el precio total
+def BuyProducts():
+    conn = sql.connect('ProyectoFinal/Users&Stock.db')
+    cursor = conn.cursor()
+    total = 0
+    boleta = []
     while True:
-        if x == 1:
-            print("Bienvenido cliente")
-            print("1. Ver productos")
-            print("2. Comprar productos")
-            print("3. Salir")
-            user = int(input("Ingrese opción: "))
-            if user == 1:
-                print("Mostrando productos...")
-            elif user == 2:
-                print("Comprando productos...")
-            elif user == 3:
-                print("Saliendo...")
-                break
-        elif x == 2:
-            print("Bienvenido administrador")
-            print("1. Agregar productos")
-            print("2. Agregar usuarios")
-            print("3. Ver inventario")
-            print("4. Salir")
-            user = int(input("Ingrese opción: "))
-            if user == 1:
-                print("Agregando productos...")
-                name = input("Ingrese nombre del producto: ")
-                price = float(input("Ingrese precio del producto: "))
-                stock = float(input("Ingrese stock del producto: "))
-                insertProduct(name, price, stock)
-            elif user == 2:
-                print("Agregando usuarios...")
-                name = input("Ingrese nombre del usuario: ")
-                password = input("Ingrese contraseña del usuario: ")
-                typeUser = input("Ingrese tipo de usuario: ")
-                insertUser(name, password, typeUser)
-            elif user == 3:
-                print("Mostrando inventario...")
-                PrintProducts()
-            elif user == 4:
-                print("Saliendo...")
-                break
+        name = input("Ingrese nombre del producto: ")
+        if name == "salir":
+            print("Saliendo...")
+            break
+        try:
+            amount = int(input("Ingrese cantidad del producto: "))
+        except ValueError:
+            print("Por favor, ingrese un valor numérico válido para la cantidad.")
+            continue
+        cursor.execute('''SELECT * FROM inventario WHERE name = ?''', (name,))
+        rows = cursor.fetchall()
+        if len(rows) == 0:
+            print("Producto no encontrado")
+        else:
+            for row in rows:
+                if row[2] < amount:
+                    print("No hay suficiente stock")
+                else:
+                    price = row[1] * amount
+                    total += price
+                    cursor.execute('''UPDATE inventario SET stock = stock - ? WHERE name = ?''', (amount, name))
+                    conn.commit()
+                    print("Compra realizada")
+                    print("El precio total es: " + str(price))
+                    boleta.append((name, amount, row[1], price))
+    if boleta:
+        print("\n----- Boleta de compra -----")
+        print("Producto\tCantidad\tPrecio\tTotal")
+        for item in boleta:
+            print(f"{item[0]}\t{item[1]}\t{item[2]}\t{item[3]}")
+        print(f"Total a pagar: {total}")
+
+#funcion para manejar las solicitudes de los clientes
+def handleClientRequests():
+    while True:
+        print("Bienvenido cliente")
+        print("1. Comprar productos")
+        print("2. Salir")
+        user = int(input("Ingrese opción: "))
+        if user == 1:
+            PrintProducts()
+            print("Comprando productos...")
+            BuyProducts()
+        elif user == 2:
+            print("Saliendo...")
+            break
+
+#funcion para manejar las solicitudes de los administradores
+def handleAdminRequests():
+    while True:
+        print("Bienvenido administrador")
+        print("1. Agregar productos")
+        print("2. Agregar usuarios")
+        print("3. Ver inventario")
+        print("4. Salir")
+        user = int(input("Ingrese opción: "))
+        if user == 1:
+            print("Agregando productos...")
+            name = input("Ingrese nombre del producto: ")
+            price = float(input("Ingrese precio del producto: "))
+            stock = float(input("Ingrese stock del producto: "))
+            insertProduct(name, price, stock)
+        elif user == 2:
+            print("Agregando usuarios...")
+            name = input("Ingrese nombre del usuario: ")
+            password = input("Ingrese contraseña del usuario: ")
+            typeUser = input("Ingrese tipo de usuario: ")
+            insertUser(name, password, typeUser)
+        elif user == 3:
+            print("Mostrando inventario...")
+            PrintProducts()
+        elif user == 4:
+            print("Saliendo...")
+            break
+
+#funcion para mostrar el menu de opciones
+#dependiendo del tipo de usuario que ingrese
+def userRequest(x):
+    if x == 1:
+        handleClientRequests()
+    elif x == 2:
+        handleAdminRequests()
+
+#funcion para validar el usuario y la contraseña
+#se conecta a la base de datos y se selecciona el usuario y la contraseña
+#si el usuario y la contraseña son correctos se devuelve el tipo de usuario
+#si el usuario y la contraseña son incorrectos se devuelve False
+def validateUser(usuario, password):
+    conn = sql.connect('ProyectoFinal/Users&Stock.db')
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM users WHERE name = ? AND password = ?''', (usuario, password))
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        print("Usuario o contraseña incorrectos")
+        conn.close()
+        return False
+    else:
+        for row in rows:
+            print("Bienvenido " + row[0])
+            conn.close()
+            return row[2]
+    
+#funcion principal donde se ingresara el usuario y validara la contraseña
+#cerrando el programa si se ingresan 3 veces mal tanto el usuario como la contraseña
 def main():
     print("Bienveniado al sistema de gestión de inventario")
-    print("1. Cliente")
-    print("2. Administrador")
-    print("3. Salir")
-    usuario = int(input("Ingrese tipo de usuario: "))
-    userRequest(usuario)
-if __name__ == "__main__":
+    intentos = 0
+    while True:
+        usuario = input("Ingrese usuario: ")
+        password = input("Ingrese contraseña: ")
+        tipo = validateUser(usuario, password)
+        if tipo == False:
+            print("Usuario o contraseña incorrectos")
+            intentos += 1
+            if intentos == 3:
+                print("Demasiados intentos")
+                print("el programa se cerrara")
+                break
+        else:
+            if tipo == "cliente":
+                userRequest(1)
+            elif tipo == "admin":
+                userRequest(2)
+delateProduct("zanahoria")
+#if __name__ == "__main__":
     #createdb('ProyectoFinal/Users&Stock.db')
     #createTable()
     #createTableUsers()
-    main()
+    
+    #main()
